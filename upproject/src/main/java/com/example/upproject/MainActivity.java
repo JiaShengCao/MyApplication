cab037e48cea08e0c205611cd8beda025706c26c
Synapsecao <986766214@qq.com>
2016-07-21 17:32:58 +0800

    changge some ui

--- upproject/src/main/java/com/example/upproject/MainActivity.java
+++ upproject/src/main/java/com/example/upproject/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -43,6 +45,13 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams
+                .FLAG_TRANSLUCENT_STATUS);
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams
+                .FLAG_TRANSLUCENT_NAVIGATION);
         setContentView(R.layout.activity_main);
         initTableLine();
         initView();
@@ -65,7 +74,7 @@ public class MainActivity extends FragmentActivity {
     //初始化控件
     public void initView(){
 
-        //初始化数据：单词运行时间/功率/开关/容忍度
+        //初始化数据：单次运行时间/功率/开关/容忍度
 
         mViewPager= (ViewPager) findViewById(R.id.viewpager);
         textView1= (TextView) findViewById(R.id.textview1);
